3c81fef593e900763b2975ba410f8d6b961cdf0d
BhumikaBadoniya <bhumikabadoniya95@gmail.com>
2024-08-01 14:51:26 +0530

    Add files via upload

---
+++ coversion.java
@@ -0,0 +1,18 @@
+public class coversion {
+    public static void main(String[] args) {
+        float c = 56.32f;
+        int a;
+        System.out.println("Float to int");
+        a = (int) c;
+        System.out.println(a);
+        
+            int m;
+            char b = 'b';
+            System.out.println("char to int");
+            m = (int) b;
+            System.out.println(m);
+        
+
+    }
+
+}
