87069c74d0a29efa43f3e6cd5f1b4b8ae89ca86f
BhumikaBadoniya <bhumikabadoniya95@gmail.com>
2024-08-07 21:46:39 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String no =sc.next();
+        System.out.println(no);
+        
+        Scanner nb = new Scanner(System.in);
+        int a =nb.nextInt();
+        int b =nb.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+
+    }
+   
+}
