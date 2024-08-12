282b73e5ca1452adf88e1ba696d8417bd1d66db7
BhumikaBadoniya <bhumikabadoniya95@gmail.com>
2024-08-12 21:06:33 +0530

    Add files via upload

---
+++ cons.java
@@ -0,0 +1,12 @@
+class fruit{
+    String s1="Mango";
+    String s2="Orange";
+}
+public class cons {
+    public static void main(String[] args) {
+        fruit o1=new fruit();
+        System.out.println(o1.s1 + " " + o1.s2);
+    }
+
+
+}
