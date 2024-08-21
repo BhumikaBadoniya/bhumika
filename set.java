fc1dbbc5d2a6a3fc08892a83641fe0df4a34f494
BhumikaBadoniya <bhumikabadoniya95@gmail.com>
2024-08-21 21:59:47 +0530

    Add files via upload

---
+++ set.java
@@ -0,0 +1,14 @@
+class set1{
+    String s1;
+    public String set(String a){
+    s1=a;
+    return s1;
+    }
+}
+public class set {
+    public static void main(String[] args) {
+        set1 o1= new set1();
+        o1.s1="Bhumika ";
+        System.out.println(o1.s1);
+    }
+}
